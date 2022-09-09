0c771c20ed4d1cddc8e3e89f7d2c038e9c2282e4
cc3389 <932043913@qq.com>
2022-09-09 16:46:50 +0800

    Initial commit

---
+++ service/gateway-service/src/main/java/edu/wit/hbasemanager/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package edu.wit.hbasemanager.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cc3389
+ * @date 2022/09/09 12:56
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
---
+++ service/user-service/src/main/java/edu/wit/hbasemanager/user/UserApplication.java
@@ -0,0 +1,15 @@
+package edu.wit.hbasemanager.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cc3389
+ * @date 2022/09/09 11:05
+ **/
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
