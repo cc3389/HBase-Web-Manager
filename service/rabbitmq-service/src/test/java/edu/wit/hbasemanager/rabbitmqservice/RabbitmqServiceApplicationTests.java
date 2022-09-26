9a9a9f33d9abeab8fccc318c2cc4690cd04ad7f7
cc3389 <932043913@qq.com>
2022-09-26 09:22:41 +0800

    完成用户微服务与消息队列微服务

---
+++ service/rabbitmq-service/src/test/java/edu/wit/hbasemanager/rabbitmqservice/RabbitmqServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wit.hbasemanager.rabbitmqservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
